be21d98c5293b71b55ac4d00d069f50f4e5b193e
Toi-Toi <52995275+Toi-Toi@users.noreply.github.com>
2019-08-07 20:58:44 +0300

    Add files via upload

---
+++ Toinka.py
@@ -0,0 +1,19 @@
+import requests
+import misc
+
+token = misc.token
+# https://api.telegram.org/bot875686930:AAFV9Smcq0JT6zgTlVgT9FBkjfIEhzASx-M/
+URL = 'https://api.telegram.org/bot' + token + '/'
+
+def get_updates():
+    url = URL + 'getupdates'
+    response = requests.get(url)
+    return response.json()
+
+
+def main():
+    dictionary = get_updates()
+
+
+
+#      'Той-Той любит пластик. Ммммм хрусть-хрусть.')
