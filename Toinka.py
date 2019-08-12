ab97390d7f23ee268fd31d2fcb7b2bd314947313
Ihor Mukha <ihor.mukha@gmail.com>
2019-08-12 21:08:05 +0300

    Игнорирование файла с токеном.

--- Toinka.py
+++ Toinka.py
@@ -1,23 +1,20 @@
-import requests
 import telebot
 import os
 
-bot = telebot.TeleBot(os.environ.get('ALL_TRIPLETS_BOT_TOKEN', None))
+token = os.environ.get('ALL_TRIPLETS_BOT_TOKEN')
 
-@bot.message_handler(commands=['start'])
-def start_message(message):
-    bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
+bot = telebot.TeleBot(token)
 
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Howdy, how are you doing?")
+
+
+
+
+
+
+
+
+# Той-Той любит пластик. Ммммм хрусть-хрусть.
 bot.polling()
-# URL = 'https://api.telegram.org/bot' + os.environ['ALL_TRIPLETS_BOT_TOKEN'] + '/'
-
-# def get_updates():
-#    url = URL + 'getupdates'
-#    response = requests.get(url)
-#    return response.json()
-#def main():
-#   dictionary = get_updates()
-
-
-
-#     f 'Той-Той любит пластик. Ммммм хрусть-хрусть.')
