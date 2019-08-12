d04fea47fb419f626fb41ca5e0cd3f10ce45daf1
Ihor Mukha <ihor.mukha@gmail.com>
2019-08-12 11:23:11 +0300

    added environmental variable 'token' to heroku

--- Toinka.py
+++ Toinka.py
@@ -1,9 +1,6 @@
 import requests
 import telebot
-import misc
 
-token = misc.token
-# https://api.telegram.org/bot875686930:AAFV9Smcq0JT6zgTlVgT9FBkjfIEhzASx-M/
 URL = 'https://api.telegram.org/bot' + token + '/'
 
 bot = telebot.TeleBot(token)
